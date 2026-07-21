269a62b98c68901d79c3b83a15437bdffdf991ea
approu <140549225+h7705679@users.noreply.github.com>
2026-07-22 02:46:32 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,17 @@
+local scripts = {
+    --[13704594433] = "",
+    [6161235818]  = "https://raw.githubusercontent.com/h7705679/Nemesis-hub/refs/heads/main/Twisted.lua"
+}
+
+local scriptUrl = scripts[game.PlaceId]
+
+if scriptUrl then
+    local success, result = pcall(function()
+        local code = game:HttpGet(scriptUrl)
+        return loadstring(code)()
+    end)
+
+    if not success then
+        warn("Error loading script: " .. tostring(result))
+    end
+end
