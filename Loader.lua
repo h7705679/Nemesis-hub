26fde7500803bef8cf60e9be51ffeb40a739fc28
approu <140549225+h7705679@users.noreply.github.com>
2026-07-22 11:44:35 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,5 @@
 local scripts = {
+    [13955927965] = "https://raw.githubusercontent.com/h7705679/my-own-skidded-HvH-script/refs/heads/main/Nemesis%20blood%20zone.lua",
     [13704594433] = "https://raw.githubusercontent.com/h7705679/Nemesis-hub/refs/heads/main/TheStorage.lua",
     [14170731342]  = "https://raw.githubusercontent.com/h7705679/Nemesis-hub/refs/heads/main/Twisted.lua" -- changed to 14170731342
 }
